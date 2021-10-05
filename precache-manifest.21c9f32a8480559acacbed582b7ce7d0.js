ad49cdef113997a0e9323232e287d23fea3b7962
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-10-06 04:00:16 +0900

    memo

---
+++ precache-manifest.21c9f32a8480559acacbed582b7ce7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e037e9ff5f9b2f4fc69",
+    "url": "/css/app.94caac71.css"
+  },
+  {
+    "revision": "078749d513b3f22da854",
+    "url": "/css/chunk-vendors.0f1ada3b.css"
+  },
+  {
+    "revision": "fba63a635d3385eb4cb57cab5cde99dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e037e9ff5f9b2f4fc69",
+    "url": "/js/app.c06c870c.js"
+  },
+  {
+    "revision": "078749d513b3f22da854",
+    "url": "/js/chunk-vendors.8f94aab7.js"
+  },
+  {
+    "revision": "ea501cb10bef945d7c89e4d1ee0c9c32",
+    "url": "/manifest.json"
+  }
+]);
